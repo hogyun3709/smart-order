a2f7c60e90df39111e676f7b1bd1b9f47a8a26c8
hogyun3709 <hogyun3709@gmail.com>
2022-02-10 21:52:47 +0900

    Practice: tailwindcss form setup with simple form practice by applying several styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TailwindForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import TailwindForm from './components/TailwindForm.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    TailwindForm,
   },
 };
 </script>
@@ -19,7 +18,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
