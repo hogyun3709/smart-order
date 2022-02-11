f1afe81c37807713732634e781e8aa62ba12473c
hogyun3709 <hogyun3709@gmail.com>
2022-02-11 10:54:03 +0900

    Feat: 기능 구현 요구사항 2

---
+++ src/views/MenuBottom.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="w-full h-screen" data-test="bottom-navigation">
+    <section
+      id="bottom-navigation"
+      class="block fixed inset-x-0 bottom-0 z-10 bh-white shadow"
+      data-test="bottom-navigation-section"
+    >
+      <div class="flex justify-between" data-test="bottom-navigation-flex">
+        <a
+          v-for="menu in menus"
+          :key="menu.text"
+          data-test="nav-menus"
+          href="#"
+          class="
+            w-full
+            focus:text-teal-500
+            hover:text-teal-500
+            justify-center
+            inline-block
+            text-center
+            pt-2
+            pb-1
+          "
+        >
+          <font-awesome-icon :class="menu.icon" data-test="nav-menu-icon" />
+          <p data-test="nav-menu-text">{{ menu.text }}</p>
+        </a>
+      </div>
+    </section>
+  </div>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menus: [
+        {
+          icon: 'fas fa-home',
+          text: '홈',
+        },
+        {
+          icon: 'fas fa-shopping-cart',
+          text: '장바구니',
+        },
+        {
+          icon: 'fa-solid fa-gear',
+          text: '설정',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
