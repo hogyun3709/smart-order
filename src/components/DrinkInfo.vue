8bef9a7da264d339bbb2a6569cfe0248b66631f5
hogyun3709 <hogyun3709@gmail.com>
2022-02-17 12:35:33 +0900

    Add: 기능구현 요구사항1번 - 하단메뉴 컴포넌트 렌더링

---
+++ src/components/DrinkInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <MenuBottom />
+</template>
+
+<script>
+import MenuBottom from '@/components/MenuBottom.vue';
+
+export default {
+  components: {
+    MenuBottom,
+  },
+};
+</script>
+
+<style>
+</style>
