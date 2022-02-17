eae8128b833825b05cbc6ded2ad898d1f52f94fd
hogyun3709 <hogyun3709@gmail.com>
2022-02-17 15:07:42 +0900

    Add: 기능구현 요구사항 2번 - 음료의 사진 표시 및 요구한 버튼들의 추가

--- src/components/DrinkInfo.vue
+++ src/components/DrinkInfo.vue
@@ -1,13 +1,64 @@
 <template>
+  <div id="drink-info" class="max-w-full mx-5" data-test="drink-info">
+    <div class="relative" data-test="drink-image-container">
+      <img
+        class="aspect-square"
+        :src="drink.image"
+        alt="drink-image"
+        data-test="drink-image"
+      />
+      <button
+        class="
+          absolute
+          top-0
+          bg-slate-400
+          text-white
+          p-2
+          rounded
+          hover:bg-blue-600
+          m-2
+        "
+        data-test="drink-image-prev-page-button"
+      >
+        <ChevronLeftIcon class="h-5 w-5" />
+      </button>
+      <button
+        class="
+          absolute
+          top-0
+          right-0
+          bg-slate-400
+          text-white
+          p-2
+          rounded
+          hover:bg-blue-600
+          m-2
+        "
+        data-test="drink-image-share-button"
+      >
+        <ShareIcon class="h-5 w-5" />
+      </button>
+    </div>
+  </div>
   <MenuBottom />
 </template>
 
 <script>
+import { ShareIcon, ChevronLeftIcon } from '@heroicons/vue/outline';
 import MenuBottom from '@/components/MenuBottom.vue';
 
 export default {
   components: {
     MenuBottom,
+    ShareIcon,
+    ChevronLeftIcon,
+  },
+  data() {
+    return {
+      drink: {
+        image: 'https://coffee.alexflipnote.dev/random',
+      },
+    };
   },
 };
 </script>
