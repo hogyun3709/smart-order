c7d26eacbcb3efcff88a648b7aa47e21e7a16586
hogyun3709 <hogyun3709@gmail.com>
2022-02-18 21:39:57 +0900

    Add: 기능구현 요구사항 8번 - 컵사이즈 옵션 선택 버튼 추가

--- src/components/DrinkInfo.vue
+++ src/components/DrinkInfo.vue
@@ -91,6 +91,20 @@
         ICE
       </button>
     </div>
+    <div data-test="drink-select-size-container">
+      <h4 class="my-3" data-test="drink-select-size-title">사이즈</h4>
+      <button
+        v-for="size in drink.sizes"
+        :key="size.text"
+        :data-test="`drink-size-${size.text}`"
+        class="w-1/4 border"
+      >
+        <p class="my-1">
+          <i :class="size.icon" :data-test="`drink-size-${size.icon}`" />
+        </p>
+        {{ size.text }}
+      </button>
+    </div>
   </div>
   <MenuBottom />
 </template>
@@ -115,6 +129,24 @@ export default {
           '풍부하고 진한 에스프레소가 신선한 스팀 밀크를 만나 부드러워진 커피 위에 우유 거품을 살짝 얹은 대표적인 카페 라떼',
         defaultPrice: 5000,
         temperature: null,
+        sizes: [
+          {
+            icon: 'fa-solid fa-mug-saucer fa-xs',
+            text: 'Short',
+          },
+          {
+            icon: 'fa-solid fa-mug-saucer fa-sm',
+            text: 'Tall',
+          },
+          {
+            icon: 'fa-solid fa-mug-saucer fa-lg',
+            text: 'Grande',
+          },
+          {
+            icon: 'fa-solid fa-mug-saucer fa-xl',
+            text: 'Venti',
+          },
+        ],
       },
     };
   },
