e1f39531baa71b4473af3558d8867c51c2a726e9
hogyun3709 <hogyun3709@gmail.com>
2022-02-23 23:26:03 +0900

    Test: 음료목록의 데이터 length에 대한 보완

--- tests/unit/ProductList.spec.js
+++ tests/unit/ProductList.spec.js
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils';
 
 import ProductList from '@/components/ProductList.vue';
+import Product from '@/components/ProductListItem.vue';
 import MenuBottom from '@/components/MenuBottom.vue';
 
 describe('기능구현 요구사항 1번', () => {
@@ -13,16 +14,17 @@ describe('기능구현 요구사항 1번', () => {
 	it('현재 표시하고있는 음료의 종류를 표시하는 공간의 존재여부를 확인합니다.', () => {
 		const drinkCategoryWrapper = wrapper.get('h2[data-test="menu-list-category-title"]');
 
-		expect(drinkCategoryWrapper.exists()).toBe(true)
+		expect(drinkCategoryWrapper.exists()).toBe(true);
 	});
 
 	it('음료의 목록이 데이터 array의 length 만큼 렌더링되는지를 확인합니다', async () => {
-		const testDrink = [{ name: '1' }, { name: '2' }]
+		const testDrink = [{ name: '1' }, { name: '2' }];
 
 		await wrapper.setData({
-			drinks: testDrink
-		})
+			drinks: testDrink,
+		});
 
-		expect(wrapper.findAll('div[data-test="product-container"]').length).toBe(testDrink.length)
-	})
+		expect(wrapper.findAll('div[data-test="product-component"]').length).toBe(testDrink.length);
+		expect(wrapper.findAllComponents(Product).length).toBe(testDrink.length)
+	});
 });
