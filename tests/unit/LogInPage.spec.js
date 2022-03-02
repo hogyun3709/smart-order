a745b1e84a13c352374c1fcc953544f381eebeb1
hogyun3709 <hogyun3709@gmail.com>
2022-03-02 21:04:48 +0900

    Test: 회원가입 버튼 추가에 따른 TC 추가

--- tests/unit/LogInPage.spec.js
+++ tests/unit/LogInPage.spec.js
@@ -2,37 +2,31 @@ import { mount } from '@vue/test-utils';
 import LogInPage from '@/views/auth/LogInPage.vue';
 
 describe('LogInPage.vue 파일에 대한 unit test 입니다', () => {
-  it('tests root container with following three required containers', () => {
-    const wrapper = mount(LogInPage);
-
-    expect(wrapper.find('div[data-test="input-container"]').exists()).toBe(true);
-    expect(wrapper.find('div[data-test="log-in-button-container"]').exists()).toBe(true);
-    expect(wrapper.find('div[data-test="show-input-text-container"]').exists()).toBe(true);
+  const wrapper = mount(LogInPage);
+  it('로그인 페이지에 대한 내용을 담을 최상단 element의 존재여부를 확인합니다', () => {
+    expect(wrapper.find('div[data-test="login-page-container"]').exists()).toBe(true);
   });
-  it('has input field for username and password', () => {
-    const wrapper = mount(LogInPage);
 
+  it('has input field for username and password', () => {
     expect(wrapper.find('input[data-test="username-input-field"]').exists()).toBeTruthy();
     expect(wrapper.find('input[data-test="password-input-field"]').exists()).toBeTruthy();
   });
-  test('로그인이라는 텍스트를 가진 버튼의 존재 여부', () => {
-    const wrapper = mount(LogInPage);
 
-    const buttonText = '로그인';
+  it('로그인과 회원가입 버튼을 렌더링할 컨테이너의 존재여부', () => {
+    expect(wrapper.find('div[data-test="log-in-or-sign-in-button-container"]').exists()).toBeTruthy();
+  });
+
+  test('로그인이라는 텍스트를 가진 버튼의 존재 여부', () => {
+    const testLogInText = '로그인';
 
     expect(wrapper.find('button[data-test="log-in-button"]').exists()).toBe(true);
-    expect(wrapper.get('button[data-test="log-in-button"]').text()).toBe(buttonText);
+    expect(wrapper.get('button[data-test="log-in-button"]').text()).toBe(testLogInText);
   });
-  it('checks v-model working properly via display text from given input values', async () => {
-    const wrapper = mount(LogInPage);
 
-    const username = 'abcd';
-    const password = 'supersecret';
+  test('회원가입이라는 텍스트를 가진 버튼의 존재여부', () => {
+    const testSignIntext = '회원가입';
 
-    await wrapper.find('input[type="text"]').setValue(username);
-    await wrapper.find('input[type="password"]').setValue(password);
-
-    expect(wrapper.find('p[data-test="show-input-username"]').text()).toContain(username);
-    expect(wrapper.find('p[data-test="show-input-password"]').text()).toContain(password);
-  });
+    expect(wrapper.find('button[data-test="sign-in-button"]').exists()).toBeTruthy();
+    expect(wrapper.get('button[data-test="sign-in-button"]').text()).toBe(testSignIntext);
+  })
 });
