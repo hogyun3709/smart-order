42e361cf2be043e8e9f13b74dc64d04e166cb566
hogyun3709 <hogyun3709@gmail.com>
2022-03-02 20:06:25 +0900

    Edit: 라우팅 수정 및 추가: Login 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,28 @@ import { createRouter, createWebHistory } from 'vue-router';
 import ProductList from '@/views/drink/ProductList.vue';
 import Cart from '@/views/cart/CartList.vue';
 import DrinkInfo from '@/views/drink/DrinkInfo.vue';
+import LogInPage from '@/views/auth/LogInPage.vue';
 
 const routes = [
   {
     path: '/',
+    name: 'Home',
+    components: { main: LogInPage },
+  },
+  {
+    path: '/products',
     name: 'ProductList',
-    components: { product_list: ProductList },
+    components: { main: ProductList },
   },
   {
     path: '/drink/testId',
     name: 'DrinkInfo',
-    components: { drink_info: DrinkInfo },
+    components: { main: DrinkInfo },
   },
   {
     path: '/cart',
     name: 'Cart',
-    components: { cart: Cart },
+    components: { main: Cart },
   },
 ];
 
