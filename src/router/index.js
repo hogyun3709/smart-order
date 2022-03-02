500e543a24ffe909424dc4c8c57e6b40a3eccccc
hogyun3709 <hogyun3709@gmail.com>
2022-03-02 21:09:55 +0900

    Add: 회원가입을 위한 라우팅 설정

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,18 @@ import ProductList from '@/views/drink/ProductList.vue';
 import Cart from '@/views/cart/CartList.vue';
 import DrinkInfo from '@/views/drink/DrinkInfo.vue';
 import LogInPage from '@/views/auth/LogInPage.vue';
+import SignInPage from '@/views/auth/SignInPage.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     components: { main: LogInPage },
+  },
+  {
+    path: '/signin',
+    name: 'SignIn',
+    components: { main: SignInPage },
   },
   {
     path: '/product',
