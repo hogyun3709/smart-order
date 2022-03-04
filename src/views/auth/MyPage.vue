ea157c9417cad7bc05151569cf28196a916d1187
hogyun3709 <hogyun3709@gmail.com>
2022-03-04 16:00:34 +0900

    Add: 마이페이지 작성

---
+++ src/views/auth/MyPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    class="grid grid-cols-6 gap-4 content-center h-full"
+    data-test="mypage-container"
+  >
+    <h2
+      class="col-start-2 col-span-4 text-xl font-bold"
+      data-test="mypage-title"
+    >
+      내 정보 조회
+    </h2>
+    <div
+      class="col-start-2 col-span-4"
+      v-if="userDetails"
+      data-test="mypage-info-container"
+    >
+      <p class="font-medium" data-test="mypage-username">
+        아이디: {{ userDetails.username }}
+      </p>
+      <p class="font-medium" data-test="mypage-nickname">
+        닉네임: {{ userDetails.nickname }}
+      </p>
+      <p class="font-medium" data-test="mypage-email">
+        이메일: {{ userDetails.email }}
+      </p>
+      <p class="font-medium" data-test="mypage-phone">
+        휴대전화: {{ userDetails.phone }}
+      </p>
+    </div>
+  </div>
+  <MenuBottom />
+</template>
+
+<script>
+import MyPageApi from '@/api/user/MyPageApi';
+import MenuBottom from '@/components/MenuBottom.vue';
+
+export default {
+  components: {
+    MenuBottom,
+  },
+  data() {
+    return {
+      userDetails: {},
+    };
+  },
+
+  methods: {},
+
+  async created() {
+    const apiClient = new MyPageApi();
+    const response = await apiClient.getUserInfo();
+    this.userDetails = response.data;
+  },
+};
+</script>
+
+<style>
+</style>
