93a4e188ae88b0e07deea223645af8dca8bdaeda
hogyun3709 <hogyun3709@gmail.com>
2022-03-04 16:00:15 +0900

    Lint: Fix Lint rules

--- tests/unit/api/SignInApi.spec.js
+++ tests/unit/api/SignInApi.spec.js
@@ -10,9 +10,9 @@ describe('회원가입 Api 호출에 대한 검증입니다', () => {
 			password: 'testPassword',
 			email: 'test@example.com',
 			phone: '01012345678',
-		}
+		};
 		const result = await new SignInApi().createUser(testPayload);
 
-		expect(result.status).toBe(SUCCESS)
-	})
-})
+		expect(result.status).toBe(SUCCESS);
+	});
+});
