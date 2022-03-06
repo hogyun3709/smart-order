79f6edc4be80795926bc83ca2206c4aec46bdf50
hogyun3709 <hogyun3709@gmail.com>
2022-03-06 19:58:21 +0900

    EsLint: rule configs change false -> 0

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'caseSensitive': false,
+    'caseSensitive': 0,
   },
   overrides: [
     {
