cb2ad09428465c6ba3be334f386d07da4e476a95
hogyun3709 <hogyun3709@gmail.com>
2022-03-06 19:19:00 +0900

    Edit: 장바구니 데이터 속성 모델화

--- src/views/Cart/CartList.vue
+++ src/views/Cart/CartList.vue
@@ -9,6 +9,7 @@
 <script>
 import CartItem from '@/components/CartListItem.vue';
 import MenuBottom from '@/components/MenuBottom.vue';
+import CartModel from '@/model/cart/cart';
 
 export default {
   components: {
@@ -18,54 +19,7 @@ export default {
 
   data() {
     return {
-      carts: [
-        {
-          id: 'E2',
-          name: '카페 라떼',
-          nameEng: 'Caffe Latte',
-          temperature: 'Hot',
-          size: 'Tall',
-          cupType: '개인 컵',
-          image: 'https://coffee.alexflipnote.dev/random',
-          personalOption: [
-            {
-              name: '에스프레소 샷',
-              defaultQuantity: 1,
-              price: 500,
-            },
-            {
-              name: '바닐라 시럽',
-              defaultQuantity: 1,
-              price: 400,
-            },
-          ],
-          defaultPrice: 5000,
-          defaultQuantity: 1,
-        },
-        {
-          id: 'E3',
-          name: '에스프레소',
-          nameEng: 'Espresso',
-          temperature: 'Hot',
-          size: 'Tall',
-          cupType: '개인 컵',
-          image: 'https://coffee.alexflipnote.dev/random',
-          personalOption: [
-            {
-              name: '에스프레소 샷',
-              defaultQuantity: 1,
-              price: 500,
-            },
-            {
-              name: '헤이즐럿 시럽',
-              defaultQuantity: 1,
-              price: 700,
-            },
-          ],
-          defaultPrice: 4000,
-          defaultQuantity: 1,
-        },
-      ],
+      carts: [CartModel],
     };
   },
   methods: {},
