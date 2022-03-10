d05e1f772378038a2da1e53f9a9749ecfea2485d
hogyun3709 <hogyun3709@gmail.com>
2022-03-10 12:25:44 +0900

    Refactor: 음료 상세페이지 컴포넌트화(퍼스널옵션 및 주문관련)

---
+++ src/components/ProductOption.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="grid grid-cols-6 grid-flow-col">
+    <div class="col-start-1 col-end-7 my-0.5">
+      <span>{{ option.name }}</span>
+      <span
+        v-if="option.unitprice"
+        class="text-gray-400 text-xs align-middle mx-4"
+        >{{ unitPriceFormat }}</span
+      >
+    </div>
+    <div class="float-right flex space-x-2 text-center my-0.5">
+      <button
+        class="border-2 border-black rounded-full w-7 h-7"
+        @click="removeShot(option.optionNo)"
+      >
+        -
+      </button>
+      <div>
+        <slot name="optionQuantity"></slot>
+      </div>
+      <button
+        class="border-2 border-black rounded-full w-7 h-7"
+        @click="addShot(option.optionNo)"
+      >
+        +
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductOption',
+  emits: ['updateOption'],
+
+  props: {
+    option: {
+      type: Object,
+      default() {
+        return {
+          name: '',
+          optionNo: -1,
+          unitprice: -1,
+          baseQuantity: -1,
+        };
+      },
+    },
+  },
+  methods: {
+    addShot(optionNo) {
+      this.$emit('updateOption', {
+        optionNo,
+        value: 1,
+      });
+    },
+    removeShot(optionNo) {
+      this.$emit('updateOption', {
+        optionNo,
+        value: -1,
+      });
+    },
+  },
+  computed: {
+    unitPriceFormat() {
+      console.log(this.option);
+      return `${this.option.unitprice.toLocaleString()}원`;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/ProductOrder.vue
@@ -0,0 +1,89 @@
+<template>
+  <div
+    class="mx-4 mb-16 grid grid-cols-6"
+    data-test="drink-draft-order-container"
+  >
+    <div class="col-start-1 col-end-7" data-test="drink-quantity-counter">
+      <button
+        class="border-2 border-black rounded-full w-8 h-8 disabled:opacity-25"
+        @click="decreQuantity()"
+        data-test="drink-counter-decrease"
+      >
+        -
+      </button>
+      <span class="mx-2" data-test="drink-quantity">
+        <slot name="drinkQuantity"></slot>
+      </span>
+      <button
+        class="border-2 border-black rounded-full w-8 h-8 disabled:opactiy-25"
+        @click="increQuantity()"
+        data-test="drink-counter-increase"
+      >
+        +
+      </button>
+    </div>
+    <div
+      class="mx-2 text-xl col-start-8 col-end-12"
+      data-test="drink-final-price"
+    >
+      <slot name="finalPrice"></slot>
+    </div>
+    <button class="my-4 col-start-1 col-end-2" data-test="drink-is-favorite">
+      <HeartIcon class="h-5 w-5" />
+    </button>
+    <div class="col-start-7 col-end-12">
+      <button
+        class="mx-2 border-2 rounded-full w-16 h-8"
+        data-test="drink-to-cart"
+        @click="handleProductToCart()"
+      >
+        담기
+      </button>
+      <button
+        class="
+          bg-emerald-400
+          hover:bg-emerald-600
+          active:bg-emerald-800
+          text-white
+          border-2
+          rounded-full
+          w-32
+          h-8
+        "
+        @click="handleOrder()"
+        data-test="drink-order"
+      >
+        주문하기
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { HeartIcon } from '@heroicons/vue/outline';
+
+export default {
+  name: 'ProductOrder',
+  emits: ['updateQuantity', 'proceedOrder', 'updateProductToCart'],
+  components: {
+    HeartIcon,
+  },
+  methods: {
+    increQuantity() {
+      this.$emit('updateQuantity', 1);
+    },
+    decreQuantity() {
+      this.$emit('updateQuantity', -1);
+    },
+    handleOrder() {
+      this.$emit('proceedOrder');
+    },
+    handleProductToCart() {
+      this.$emit('updateProductToCart');
+    },
+  },
+};
+</script>
+
+<style>
+</style>
