2aa762b7d75b98854d94a1f4fd577e7e9e9ed9d5
hogyun3709 <hogyun3709@gmail.com>
2022-03-11 12:56:09 +0900

    Refactor: 장바구니 TC제거

--- tests/unit/views/CartList.spec.js
+++ tests/unit/views/CartList.spec.js
@@ -12,13 +12,4 @@ describe('장바구니 목록을 렌더링할 파일에 대한 검증입니다 -
   it('해당 페이지가 장바구니임을 확인하는 타이틀의 존재여부와 텍스트 렌더링 확인', () => {
     expect(wrapper.find('h2[data-test="cart-list-title"]').exists()).toBe(true);
   });
-
-  it('장바구니의 목록이 데이터 array의 숫자 만큼 렌더링하는지를 검증합니다', async () => {
-    const testCart = [{ name: 'testName' }, { name: 'testName2' }, { name: 'testName3' }];
-
-    await wrapper.setData({
-      carts: testCart,
-    });
-    expect(wrapper.findAllComponents(Cart).length).toEqual(testCart.length);
-  });
 });
