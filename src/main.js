cdf122f1c0fa63b51939a91a0768af088645b74c
hogyun3709 <hogyun3709@gmail.com>
2022-03-11 21:53:52 +0900

    Feat: using api client instance as global config

--- src/main.js
+++ src/main.js
@@ -2,5 +2,22 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './index.css';
+import BaseClient from './api/BaseClient';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+const client = new BaseClient();
+
+client.instance.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    if (error.response.status === 403) {
+      await router.push('/');
+    }
+    return error;
+  },
+);
+
+app.use(router);
+app.mount('#app');
+app.config.globalProperties.apiClient = client;
