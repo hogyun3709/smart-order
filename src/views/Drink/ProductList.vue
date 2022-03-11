c640d790bece78c5bedaccb83d3892ce01f16e75
hogyun3709 <hogyun3709@gmail.com>
2022-03-11 14:02:55 +0900

    Feat: Improve UX by adding loading component

--- src/views/Drink/ProductList.vue
+++ src/views/Drink/ProductList.vue
@@ -3,13 +3,22 @@
     <h2 class="text-xl font-bold" data-test="menu-list-category-title">
       에스프레소
     </h2>
-    <router-link
-      v-for="drink in drinks"
-      :key="drink.productNo"
-      :to="drinkNoPath(drink.productNo)"
-    >
-      <Product v-bind="drink" class="mx-3 my-3" data-test="product-component" />
-    </router-link>
+    <div v-if="!isLoading">
+      <router-link
+        v-for="drink in drinks"
+        :key="drink.productNo"
+        :to="drinkNoPath(drink.productNo)"
+      >
+        <Product
+          v-bind="drink"
+          class="mx-3 my-3"
+          data-test="product-component"
+        />
+      </router-link>
+    </div>
+    <div v-else>
+      <LoadingProduct v-for="i in 2" :key="i"></LoadingProduct>
+    </div>
   </div>
   <MenuBottom />
 </template>
@@ -19,15 +28,18 @@ import MenuBottom from '@/components/MenuBottom.vue';
 import Product from '@/components/ProductListItem.vue';
 import DrinkApi from '@/api/drink/DrinkApi';
 import ProductsModel from '@/model/drink/products';
+import LoadingProduct from '@/components/Loading/LoadingProduct.vue';
 
 export default {
   components: {
     MenuBottom,
     Product,
+    LoadingProduct,
   },
   data() {
     return {
       drinks: [ProductsModel],
+      isLoading: true,
     };
   },
   methods: {
@@ -40,6 +52,7 @@ export default {
     const response = await apiClient.getProducts();
     const originalDrinks = this.drinks;
     this.drinks = Object.assign(originalDrinks, response.data.products);
+    this.isLoading = false;
   },
 };
 </script>
