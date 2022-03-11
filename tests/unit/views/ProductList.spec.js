72e906fe35e9bf9f018da5ca7fdcf5f8cfedbef2
hogyun3709 <hogyun3709@gmail.com>
2022-03-11 18:15:12 +0900

    Test: remove commented TC

--- tests/unit/views/ProductList.spec.js
+++ tests/unit/views/ProductList.spec.js
@@ -1,29 +1,8 @@
 import { mount } from '@vue/test-utils';
-// import { createRouter, createWebHistory } from 'vue-router';
-// import baseRoutes from '@/router/base';
-// import drinkRoutes from '@/router/drink';
-import Product from '@/components/ProductListItem.vue';
 import MenuBottom from '@/components/MenuBottom.vue';
 import ProductList from '@/views/Drink/ProductList.vue';
 
-// const finalRoute = [].concat(baseRoutes, drinkRoutes);
-
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   routes: finalRoute,
-// });
-
-// const mockGetProducts = jest.fn();
-// const mockGetProductDetail = jest.fn();
-// jest.mock('@/api/drink/DrinkApi', () => jest.fn().mockImplementation(() => ({
-//   getProducts: mockGetProducts, getProductDetail: mockGetProductDetail
-// })))
-
 describe('기능구현 요구사항 1번', () => {
-  // beforeEach(() => {
-  //   mockGetProducts.mockClear();
-  // })
-
   const wrapper = mount(ProductList);
 
   it('메뉴 부분의 표시가 이루어지는지 테스트 합니다.', () => {
@@ -35,5 +14,4 @@ describe('기능구현 요구사항 1번', () => {
 
     expect(drinkCategoryWrapper.exists()).toBe(true);
   });
-
 });
