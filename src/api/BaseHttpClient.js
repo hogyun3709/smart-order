0beba1cf6e919883d2139d30bc5e8069bffab63d
hogyun3709 <hogyun3709@gmail.com>
2022-03-11 18:25:13 +0900

    Refactor: Basic auth와 Bearer auth handling

---
+++ src/api/BaseHttpClient.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://taling.projectlion.lkaybob.pe.kr/api';
+
+class BaseClient {
+	#ACCESS_TOKEN = '';
+
+	#CLIENT_ID = 'web-app';
+
+	#CLIENT_SECRET = 'abcd1234';
+
+	#instance = undefined;
+
+	constructor() {
+		this.#instance = axios.create({
+			baseURL: BASE_URL,
+		});
+	}
+
+	getBasicAuth() {
+		const SOURCE_STRING = `${this.#CLIENT_ID}:${this.#CLIENT_SECRET}`;
+		return window.btoa(SOURCE_STRING);
+	}
+
+	get instance() {
+		this.#instance.defaults.headers.authorization = this.#ACCESS_TOKEN !== '' ? `Bearer ${this.#ACCESS_TOKEN}` : `Basic ${this.getBasicAuth()}`;
+
+		return this.#instance;
+	}
+
+	set ACCESS_TOKEN(accessToken) {
+		this.#ACCESS_TOKEN = accessToken;
+	}
+}
+
+export default BaseClient;
