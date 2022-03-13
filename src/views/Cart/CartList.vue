c0fe2b05f33dc83b6a6ae9cbc63d42da0ccb145a
hogyun3709 <hogyun3709@gmail.com>
2022-03-13 22:08:53 +0900

    Edit: 장바구니 아이템의 최종가격 변경

--- src/views/Cart/CartList.vue
+++ src/views/Cart/CartList.vue
@@ -1,35 +1,47 @@
 <template>
   <div class="mx-3 my-3" data-test="cart-list-container">
     <h2 class="text-xl font-bold" data-test="cart-list-title">장바구니</h2>
-    <CartItem
-      v-for="cart in carts"
-      :key="cart.product.productNo"
-      v-bind="cart"
-    />
+    <div v-if="!isLoading">
+      <CartItem
+        v-for="(cart, index) in getCartItems"
+        :key="index"
+        v-bind="cart"
+      />
+    </div>
+    <div v-else>
+      <LoadingProduct></LoadingProduct>
+    </div>
     <MenuBottom />
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import CartItem from '@/components/CartListItem.vue';
 import MenuBottom from '@/components/MenuBottom.vue';
 import CartApi from '@/api/order/CartApi';
+import LoadingProduct from '@/components/Loading/LoadingProduct.vue';
 
 export default {
   components: {
     CartItem,
     MenuBottom,
+    LoadingProduct,
   },
 
   data() {
     return {
-      carts: [],
+      isLoading: true,
     };
+  },
+  computed: {
+    ...mapGetters(['getCartItems']),
   },
   async created() {
     const apiClient = new CartApi(this.apiClient);
     const response = await apiClient.getProductsInCart();
-    this.carts = response.data.cart;
+    this.$store.dispatch('setCartItem', response.data.cart);
+    this.isLoading = false;
   },
 };
 </script>
