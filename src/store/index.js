89e52989cfab331363e120c3a90b2775cb1beec3
hogyun3709 <hogyun3709@gmail.com>
2022-03-14 00:13:20 +0900

    Add: store 디벨롬

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,15 @@ import { createStore } from 'vuex';
 export default createStore({
   state: {
     access_token: '',
+    cart_items: [],
+    product_to_cart: [],
   },
   getters: {
     getAccessToken(state) {
       return state.access_token;
+    },
+    getCartItems(state) {
+      return state.cart_items;
     },
   },
   mutations: {
@@ -16,6 +21,16 @@ export default createStore({
     CLEAR_TOKEN(state) {
       state.access_token = undefined;
     },
+    SET_CART_ITEM(state, item) {
+      const arr = [];
+      state.cart_items = arr.concat(item);
+    },
+    ADD_PRODUCT_TO_CART(state, product) {
+      state.product_to_cart.push(product);
+    },
+    INCRE_QUANTITY(state, index) {
+      state.cart_items[index].quantity += 1;
+    },
   },
   actions: {
     setToken({ commit }, token) {
@@ -23,6 +38,12 @@ export default createStore({
     },
     clearToken({ commit }) {
       commit('CLEAR_TOKEN');
+    },
+    setCartItem({ commit }, item) {
+      commit('SET_CART_ITEM', item);
+    },
+    addProductToCart({ commit }, product) {
+      commit('ADD_PRODUCT_TO_CART', product);
     },
   },
 });
